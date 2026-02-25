59a8c96ae17a09bc641223fc61fe4e55c66a6ab8
alma23nz <149969050+alma23nz@users.noreply.github.com>
2026-02-25 15:42:04 +0100

    lists uploaded

--- public/index.js
+++ public/index.js
@@ -1,1 +1,27 @@
 console.log('Hello, world!') ;
+fetch("stores.json")
+.then(response=>response.json())
+.then(stores => {
+    console.log(stores)
+
+    const container =document.getElementById("container");
+
+    stores.forEach(item => {
+
+        let url;
+if (item.url.startsWith("http")) {
+    url = item.url;
+} else {
+    url = "https://" + item.url;
+}
+
+        const div = document.createElement("div");
+        div.innerHTML= `
+        <h3>${item.name}</h3>
+        <a href="${item.url}" target="_blank">Visit Store</a>
+        <p>${item.district}</p>
+        `;
+        container.appendChild(div);
+    });
+})
+.catch(error=>console.error(error))
