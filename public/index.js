7a7191d31ee998e5b8426787b2fe5bbe875b2e71
hamda-02 <saha23hm@student.ju.se>
2026-02-25 15:45:51 +0100

    2nd upload

--- public/index.js
+++ public/index.js
@@ -1,27 +1,27 @@
-console.log('Hello, world!') ;
+console.log("Hello, world!");
 fetch("stores.json")
-.then(response=>response.json())
-.then(stores => {
-    console.log(stores)
+  .then((response) => response.json())
+  .then((stores) => {
+    console.log(stores);
 
-    const container =document.getElementById("container");
+    const container = document.getElementById("container");
 
-    stores.forEach(item => {
+    stores.forEach((item) => {
+      let url;
 
-        let url;
-if (item.url.startsWith("http")) {
-    url = item.url;
-} else {
-    url = "https://" + item.url;
-}
+      if (item.url.startsWith("http")) {
+        url = item.url;
+      } else {
+        url = "https://" + item.url;
+      }
 
-        const div = document.createElement("div");
-        div.innerHTML= `
+      const div = document.createElement("div");
+      div.innerHTML = `
         <h3>${item.name}</h3>
-        <a href="${item.url}" target="_blank">Visit Store</a>
+        <a href="${url}" target="_blank">Visit Store</a>
         <p>${item.district}</p>
         `;
-        container.appendChild(div);
+      container.appendChild(div);
     });
-})
-.catch(error=>console.error(error))
+  })
+  .catch((error) => console.error(error));
