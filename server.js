1e498e40960ed22321e73c0146e9a36adcbf169c
alma23nz <149969050+alma23nz@users.noreply.github.com>
2026-03-01 12:14:12 +0100

    api/stores
    now the api for stores work

--- server.js
+++ server.js
@@ -4,12 +4,28 @@ const PORT = 3000;
 const sqlite3 =require ("sqlite3")
 const cors = require("cors")
 
+const stores = require("./stores.json");
 
 app.use(express.json());   
 app.use(express.static("public"));
 
 app.get("/" , (req , res)=>{
     res.send("welcom to the rest API");
+});
+
+app.get("/api/stores", (req, res) => {
+  res.json(stores);
+});
+app.get("/api/products/:id", (req,res)=>{
+    let storesID= parseInt(req.params.id);
+    let store = stores.find(p=>p.id === storesID);
+
+    if(store){
+        res.json(store);
+    }else{
+        res.status(404).json({message: "store not found"});
+    }
+
 });
 
 
