368ea36663084f4b85d531193b95f1ccd2e3bd41
alma23nz <149969050+alma23nz@users.noreply.github.com>
2026-03-05 14:03:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const sqlite3 = require("sqlite3")
 const cors = require("cors")
 
 const stores = require("./stores.json");
